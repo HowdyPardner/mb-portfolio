e85502f331592868b672446897571556df039241
Marco Bragado <msb.misc@gmail.com>
2023-11-07 20:20:01 -0800

    changing output js file name

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './js/scripts.js',
     output: {
-        filename: 'bundle.js', // Specify the output filename
-        path: path.resolve(__dirname, 'dist'), // Specify the output directory as 'dist'
+        filename: 'main.js', 
+        path: path.resolve(__dirname, 'dist'), 
     },
 };
