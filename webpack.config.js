ee3ca28aa417f0fae409f3dfc7794c5ab47e77b1
Marco Bragado <msb.misc@gmail.com>
2023-11-07 20:17:52 -0800

    adding dist to html file

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,8 @@ const path = require('path');
 
 module.exports = {
     entry: './js/scripts.js',
+    output: {
+        filename: 'bundle.js', // Specify the output filename
+        path: path.resolve(__dirname, 'dist'), // Specify the output directory as 'dist'
+    },
 };
